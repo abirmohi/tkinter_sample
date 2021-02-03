70fbde7bef8a1ff0904862a6074984885cf0acb1
AP19063 モヒウッディン 　 <ap19063@azec.ac.jp>
2021-02-03 13:53:45 +0900

    add comment to tk13

--- tk.13.py
+++ tk.13.py
@@ -1,7 +1,9 @@
+#tk13.pyw
+#modify
 import tkinter as tk
 
 root = tk.Tk()
-root.geometry('300x200')
+root.geometry('350x200')
 lb = tk.Label (text='this is a label,this is a label,this is a label')
 ms = tk. Message(text='this is a message .this is a message.this is a message')
 [widget.pack() for widget in (lb,ms)]
